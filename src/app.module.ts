4c202a35b70ed8c266c89a9f2b1afacdb8f432e9
Solpio <ssusolpio@gmail.com>
2023-07-20 08:30:42 +0400

    setup project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { BotModule } from './bot/bot.module';
 
 @Module({
-  imports: [],
+  imports: [BotModule],
   controllers: [],
   providers: [],
 })
